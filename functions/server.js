4a689004dfb8664bfcf73ab786f1ddee84a5b73b
Sumit Yadav <sumit1996926@gmail.com>
2023-08-12 03:43:59 +0530

    handeling errors

--- functions/server.js
+++ functions/server.js
@@ -1,20 +1,68 @@
-const { handler } = require("./server");
-const router = express.Router();
+// const express = require("express");
+// // const { handler } = require("./functions/server");
+// const router = express.Router();
+// const cors = require("cors");
+// const nodemailer = require("nodemailer");
+
+// // server used to send send emails
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// server.use("/", router);
+// exports.handler = serverless(app);
+
+// const contactEmail = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: "skywebsoln@gmail.com",
+//     pass: "toztrkmwkfzephoh",
+//   },
+// });
+
+// contactEmail.verify((error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Ready to Send");
+//   }
+// });
+
+// router.post("/contact", (req, res) => {
+//   const name = req.body.firstName + req.body.lastName;
+//   const email = req.body.email;
+//   const message = req.body.message;
+//   const phone = req.body.phone;
+//   const mail = {
+//     from: name,
+//     to: "skywebsoln@gmail.com",
+//     subject: "Contact Form Submission - Portfolio",
+//     html: `<p>Name: ${name}</p>
+//            <p>Email: ${email}</p>
+//            <p>Phone: ${phone}</p>
+//            <p>Message: ${message}</p>`,
+//   };
+//   contactEmail.sendMail(mail, (error) => {
+//     if (error) {
+//       res.json(error);
+//     } else {
+//       res.json({ code: 200, status: "Message Sent" });
+//     }
+//   });
+// });
+const express = require("express");
+const { json } = require("body-parser");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
-// server used to send send emails
 const app = express();
 app.use(cors());
-app.use(express.json());
-server.use("/", router);
-exports.handler = serverless(app);
+app.use(json());
 
 const contactEmail = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "skywebsoln@gmail.com",
-    pass: "toztrkmwkfzephoh",
+    pass: "toztrkmwkfzephoh", // Make sure to add your actual password here
   },
 });
 
@@ -26,7 +74,7 @@ contactEmail.verify((error) => {
   }
 });
 
-router.post("/contact", (req, res) => {
+app.post("/contact", (req, res) => {
   const name = req.body.firstName + req.body.lastName;
   const email = req.body.email;
   const message = req.body.message;
@@ -48,3 +96,5 @@ router.post("/contact", (req, res) => {
     }
   });
 });
+
+exports.handler = app;
